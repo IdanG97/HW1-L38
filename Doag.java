16d65368568e9ad81eba970f8e42195e34c6e148
IdanG97 <138139373+IdanG97@users.noreply.github.com>
2025-01-12 10:44:52 +0200

    Create Doag.java

---
+++ Doag.java
@@ -0,0 +1,15 @@
+class Dog extends Animal {
+    public Dog(String name) {
+        super(name);
+    }
+
+    @Override
+    public void greets() {
+        System.out.println("Woof");
+    }
+
+    public void greets(Dog anotherDog) {
+        System.out.println("Wooooof");
+    }
+}
+
